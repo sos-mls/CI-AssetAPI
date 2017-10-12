77d43b06c0dd8cddab2809897ee83db752f0e8d2
Christian Micklisch <christian.micklisch@successwithsos.com>
2017-10-12 12:29:37 -0400

    Updated the API update

--- push-to-server.sh
+++ push-to-server.sh
@@ -3,12 +3,12 @@
 # setup ssh information.
 . "helpers/get-ssh-info.sh"
 
-COMMANDS[0]="echo '${SSH_PASSWORD}' | sudo -S rm -rf assetapi"
+COMMANDS[0]="sudo -S rm -rf assetapi"
 COMMANDS[1]="git clone https://bitbucket.org/scooblyboo/assetapi.git"
-COMMANDS[2]="echo '${SSH_PASSWORD}' | sudo rsync -a assetapi/src/ /var/www/asset_api/src/"
-COMMANDS[3]="echo '${SSH_PASSWORD}' | sudo rsync -a assetapi/vagrant/ install/"
-COMMANDS[4]="echo '${SSH_PASSWORD}' | sudo rsync -a assetapi/sql/ sql/"
-COMMANDS[5]="echo '${SSH_PASSWORD}' | sudo chown www-data:www-data -R /var/www/asset_api"
+COMMANDS[2]="sudo rsync -a assetapi/src/ /var/www/asset_api/src/"
+COMMANDS[3]="sudo rsync -a assetapi/vagrant/ install/"
+COMMANDS[4]="sudo rsync -a assetapi/sql/ sql/"
+COMMANDS[5]="sudo chown www-data:www-data -R /var/www/asset_api"
 COMMANDS[6]="bash install/composer.sh"
 
 n_elements=${#COMMANDS[@]}

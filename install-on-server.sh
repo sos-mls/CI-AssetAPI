d4f7ffc86aeeab082ef74a6a319157aa2910e5ed
Christian Micklisch <christian.micklisch@successwithsos.com>
2017-10-12 20:56:03 -0400

    Added in tracking the mysql username and password

--- install-on-server.sh
+++ install-on-server.sh
@@ -3,6 +3,14 @@
 # setup ssh information.
 . "helpers/get-ssh-info.sh"
 
+echo -n "Username for MySQL: "
+read -s ASSETAPI_MYSQL_USERNAME
+echo
+
+echo -n "Password for MySQL: "
+read -s ASSETAPI_MYSQL_PASSWORD
+echo
+
 COMMANDS[0]="echo '${SSH_PASSWORD}' | sudo -S apt-get -y --force-yes install git"
 COMMANDS[1]="git clone https://bitbucket.org/scooblyboo/assetapi.git"
 COMMANDS[2]="sudo mkdir /var/www"
@@ -10,10 +18,13 @@ COMMANDS[3]="sudo mkdir /var/www/assetapi"
 COMMANDS[4]="sudo mv assetapi/src /var/www/asset_api/src"
 COMMANDS[5]="mv assetapi/vagrant install"
 COMMANDS[6]="mv assetapi/sql sql"
+# Configure Installtion process
 COMMANDS[7]="sed -i 's/\/home\/vagrant/\/root/g' install/install.sh"
-COMMANDS[8]="bash install/install.sh"
-COMMANDS[9]="sudo mkdir /var/www/asset_api/src/assets"
-COMMANDS[10]="sudo chown www-data:www-data -R /var/www/asset_api/src"
+COMMANDS[8]="sed -i 's/default_username/${ASSETAPI_MYSQL_USERNAME}/g' install/helpers/config.txt"
+COMMANDS[9]="sed -i 's/default_password/${ASSETAPI_MYSQL_PASSWORD}/g' install/helpers/config.txt"
+COMMANDS[10]="bash install/install.sh"
+COMMANDS[11]="sudo mkdir /var/www/asset_api/src/assets"
+COMMANDS[12]="sudo chown www-data:www-data -R /var/www/asset_api/src"
 
 n_elements=${#COMMANDS[@]}
 

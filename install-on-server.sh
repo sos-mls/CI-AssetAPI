9116b664bcc7a25cbdd99309f3ed817e549e74ec
Christian Micklisch <christian.micklisch@successwithsos.com>
2017-10-12 21:18:50 -0400

    added some commenting to the installation process

--- install-on-server.sh
+++ install-on-server.sh
@@ -18,10 +18,13 @@ COMMANDS[3]="sudo mkdir /var/www/assetapi"
 COMMANDS[4]="sudo mv assetapi/src /var/www/asset_api/src"
 COMMANDS[5]="mv assetapi/vagrant install"
 COMMANDS[6]="mv assetapi/sql sql"
+
 # Configure Installtion process
 COMMANDS[7]="sed -i 's/\/home\/vagrant/\/root/g' install/install.sh"
 COMMANDS[8]="sed -i 's/default_username/${ASSETAPI_MYSQL_USERNAME}/g' install/helpers/config.txt"
 COMMANDS[9]="sed -i 's/default_password/${ASSETAPI_MYSQL_PASSWORD}/g' install/helpers/config.txt"
+
+# Run Installation
 COMMANDS[10]="bash install/install.sh"
 COMMANDS[11]="sudo mkdir /var/www/asset_api/src/assets"
 COMMANDS[12]="sudo chown www-data:www-data -R /var/www/asset_api/src"
